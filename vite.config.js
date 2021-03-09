9a9b3c6e20456e6ba2ea8add623d53cb16fd0955
zhangwencai <zhangwencai@urplus.cn>
2021-03-09 07:33:12 +0000

    配置 alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'comps': path.resolve(__dirname, 'src/components'),
+      'apis': path.resolve(__dirname, 'src/apis'),
+      'views': path.resolve(__dirname, 'src/views'),
+      'routes': path.resolve(__dirname, 'src/routes'),
+      'utils': path.resolve(__dirname, 'src/utils'),
+      'styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
   plugins: [vue()]
 })
