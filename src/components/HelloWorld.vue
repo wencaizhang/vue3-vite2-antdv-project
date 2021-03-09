eddbe0101c479ade3e4b39b786597527851f8b51
wencaizhang <1052642137@qq.com>
2021-03-09 08:18:01 +0000

    feat: 整合 antdv ui 组件库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,7 @@
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
+  <a-button @click="state.count++">count is: {{ state.count }}</a-button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
 import router from './router';
 import './styles/index.less';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(Antd).use(router).mount('#app')
